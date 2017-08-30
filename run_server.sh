75225a670472105bd81e8838d492c44d9aff373e
andy <penolove15@gmail.com>
2017-08-30 14:50:51 +0800

    201709

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 nvidia-docker run --shm-size="4g"  --net host --name gpu_s \
         -v /home/stream/Documents/webcam_img_capture/:/home/stream/Documents/webcam_img_capture/ \
-        -d penolove/cuda:201708_MAS tail -f /dev/null
+        -d penolove/cuda:201709_MAS tail -f /dev/null
+
 #sudo ./pipework.sh
